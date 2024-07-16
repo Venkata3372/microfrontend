5bd2bf4e2457ec70f6112e9e277e9f2ebeeadbd3
Venkata Lakshmi Narayana Chenna <lakshminarayana.chenna@artifixel.com>
2024-07-16 20:18:11 +0530

    first commit

---
+++ sub-app1/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="welcome-text font1">Welcome to Artifixel Technologies</h1>
+      <h1 className="welcome-text font2">Welcome to Artifixel Technologies</h1>
+      <h1 className="welcome-text font3">Welcome to Artifixel Technologies</h1>
+      <h1 className="welcome-text font4">Welcome to Artifixel Technologies</h1>
+      <h1 className="welcome-text font5">Welcome to Artifixel Technologies</h1>
+    </div>
+  );
+}
+
+export default App;
